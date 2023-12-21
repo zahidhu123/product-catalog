86f86a3485a97803310e1e1f7a471b4e2a670043
unknown <zahidmd1996@gmail.com>
2023-12-21 23:47:32 +0530

    file-added

---
+++ lecture26/index.js
@@ -0,0 +1,30 @@
+// var x = 7;
+// function getName() {
+//     console.log("Namaste Javascript")
+// }
+ 
+// getName();
+// console.log(x)
+
+// Function Tutorial
+
+var x = 1;
+
+console.log(x)
+
+function a(){
+    var a = 10;
+    console.log(x)
+}
+
+function b() {
+    var a = 100;
+    console.log(x)
+}
+
+a();
+b();
+
+
+
+
